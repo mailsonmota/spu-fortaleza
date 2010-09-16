785277dc76d8474a39091f0d32a4767ee608b1e5
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-09-16 16:50:10 +0000

    Modificação no modo como o sistema puxa o detalhes do usuário logado

--- application/controllers/UsuarioController.php
+++ application/controllers/UsuarioController.php
@@ -1,8 +1,12 @@
 <?php
+
+Loader::loadAlfrescoApiClass('AlfrescoPeople');
+Loader::loadEntity('Usuario');
 class UsuarioController extends BaseController
 {
     public function cadastroAction()
     {
-        
+    	$authInstance = Zend_Auth::getInstance()->getIdentity();
+        $this->view->person = $authInstance['user'];
     }
 }
---
+++ library/spu/entities/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('BaseAlfrescoEntity.php');
+Loader::loadAlfrescoApiClass('AlfrescoPeople');
+
+class Usuario extends BaseAlfrescoEntity
+{
+    private $_alfUsuarioObject;
+    
+    public function __construct($baseUrl, $ticket) {
+        $this->_alfUsuarioObject = new AlfrescoPeople($baseUrl, $ticket); 
+    }
+    
+    public function getPerson($userName) {
+        return $this->_alfUsuarioObject->getPerson($userName);
+    }
+}
