ca4f27844ac21e67a98fb283e5ebcbdb3eacb896
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-11-05 15:43:46 +0000

    modificação da função de encode de json

--- library/Alfresco/API/CurlClient/PostAdapters/JsonPostAdapter.php
+++ library/Alfresco/API/CurlClient/PostAdapters/JsonPostAdapter.php
@@ -4,21 +4,21 @@ class JsonPostAdapter extends JsonGetAdapter
 {
     public function encode($data) {
         // TODO ver função json_encode()
-        $json = "{";
-        
-        $count = count($data);
-        
-        $i = 1;
-        foreach($data as $key => $value) {
-            $json .= "\"" . $key . "\" : \"" . $value . "\"";
-            if ($i < $count) {
-                $json .= ", ";
-            }
-            $i++;
-        }
-        
-        $json .= "}";
-        
+//        $json = "{";
+//        
+//        $count = count($data);
+//        
+//        $i = 1;
+//        foreach($data as $key => $value) {
+//            $json .= "\"" . $key . "\" : \"" . $value . "\"";
+//            if ($i < $count) {
+//                $json .= ", ";
+//            }
+//            $i++;
+//        }
+//        
+//        $json .= "}";
+        $json = json_encode($data);
         return $json;
     }
     
