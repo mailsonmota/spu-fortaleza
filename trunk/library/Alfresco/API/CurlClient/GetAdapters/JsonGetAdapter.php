4a1562aafd31a600ae0dbfa6c08be038b83e1207
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-11-11 14:37:16 +0000

    json get adaptar: método json decode agora permite segundo argumento para passar true ou false para o assoc. padrão é o false

--- trunk/library/Alfresco/API/CurlClient/GetAdapters/JsonGetAdapter.php
+++ trunk/library/Alfresco/API/CurlClient/GetAdapters/JsonGetAdapter.php
@@ -2,8 +2,8 @@
 
 class JsonGetAdapter
 {
-    public function decode($data) {
+    public function decode($data, $assoc = false) {
         // TODO fazer ou procurar função que valide json
-        return json_decode($data);
+        return json_decode($data, $assoc);
     }
 }
