96d06809bc0a28a33c87589448d986cd8357aa2e
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-12-02 18:04:54 +0000

    inclusão de método que retorna array com ufs

--- application/controllers/BaseController.php
+++ application/controllers/BaseController.php
@@ -110,4 +110,37 @@ abstract class BaseController extends Zend_Controller_Action
         $user = AuthPlugin::getIdentity();
         return $user['ticket'];
     }
+    
+    protected function _getListaUfs()
+    {
+    	return array(
+    	   'AC' => 'AC',
+    	   'AL' => 'AL',
+    	   'AM' => 'AM',
+    	   'AP' => 'AP',
+    	   'BA' => 'BA',
+    	   'CE' => 'CE',
+    	   'DF' => 'DF',
+    	   'ES' => 'ES',
+    	   'GO' => 'GO',
+    	   'MA' => 'MA',
+    	   'MG' => 'MG',
+    	   'MS' => 'MS',
+    	   'MT' => 'MT',
+    	   'PA' => 'PA',
+    	   'PB' => 'PB',
+    	   'PE' => 'PE',
+    	   'PI' => 'PI',
+    	   'PR' => 'PR',
+    	   'RJ' => 'RJ',
+    	   'RN' => 'RN',
+    	   'RO' => 'RO',
+    	   'RR' => 'RR',
+    	   'RS' => 'RS',
+    	   'SC' => 'SC',
+    	   'SE' => 'SE',
+    	   'SP' => 'SP',
+    	   'TO' => 'TO'
+    	);
+    }
 }
