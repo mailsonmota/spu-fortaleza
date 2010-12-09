346c7b33fde3a94dad4f84e2f2bc0d2b3eb74060
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-12-09 18:14:57 +0000

    remoção de comentário

--- library/Alfresco/API/CurlClient/PostAdapters/JsonPostAdapter.php
+++ library/Alfresco/API/CurlClient/PostAdapters/JsonPostAdapter.php
@@ -3,21 +3,6 @@
 class JsonPostAdapter extends JsonGetAdapter
 {
     public function encode($data) {
-        // TODO ver função json_encode()
-//        $json = "{";
-//        
-//        $count = count($data);
-//        
-//        $i = 1;
-//        foreach($data as $key => $value) {
-//            $json .= "\"" . $key . "\" : \"" . $value . "\"";
-//            if ($i < $count) {
-//                $json .= ", ";
-//            }
-//            $i++;
-//        }
-//        
-//        $json .= "}";
         $json = json_encode($data);
         return $json;
     }
