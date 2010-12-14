fe11710879f5318184108e21296e8b7df7c20784
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-12-14 18:56:52 +0000

    codificação das interfaces dos adaptadores do curlclient e implementação delas nos adaptadores

--- trunk/library/Alfresco/API/CurlClient/GetAdapters/JsonGetAdapter.php
+++ trunk/library/Alfresco/API/CurlClient/GetAdapters/JsonGetAdapter.php
@@ -1,9 +1,11 @@
 <?php
 
-class JsonGetAdapter
+require_once('GetAdaptersInterface.php');
+
+class JsonGetAdapter implements getAdapter
 {
     public function decode($data, $assoc = false) {
-        // TODO fazer ou procurar função que valide json
+        // FIXME fazer ou procurar função que valide json
         return json_decode($data, $assoc);
     }
 }
