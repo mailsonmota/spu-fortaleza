9e7e4865bbed0babb4158486482b6e2f5337ddc4
claudemirton <claudemirton@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-12-17 20:06:36 +0000

    Edição de nó de protocolo

--- trunk/application/controllers/ProtocolosController.php
+++ trunk/application/controllers/ProtocolosController.php
@@ -2,27 +2,39 @@
 Loader::loadEntity('Protocolo');
 class ProtocolosController extends BaseController
 {
-    public function indexAction()
-    {
-    	$protocolo = new Protocolo($this->getTicket());
-        $listaProtocolos = $protocolo->listarTodos();
-        $this->view->lista = $listaProtocolos;
-    }
-    
-    public function editarAction()
-    {
-        $id = $this->_getIdFromUrl();
-        
-        $protocolo = new Protocolo($this->getTicket());
-        $protocolo->carregarPeloId($id);
-        
-        $this->view->protocolo = $protocolo;
-        $this->view->id = $protocolo->getId();
-    }
-    
-    private function _getIdFromUrl()
-    {
-        $id = $this->getRequest()->getParam('id');
-        return $id;
-    }
+	public function indexAction()
+	{
+		$protocolo = new Protocolo($this->getTicket());
+		$listaProtocolos = $protocolo->listarTodos();
+		$this->view->lista = $listaProtocolos;
+	}
+
+	public function editarAction()
+	{
+		$id = $this->_getIdFromUrl();
+
+		$protocolo = new Protocolo($this->getTicket());
+		$protocolo->carregarPeloId($id);
+
+		if ($this->getRequest()->isPost()) {
+			$postData = $this->getRequest()->getPost();
+			if (!isset($postData['recebeMalotes'])) {
+				$postData['recebeMalotes'] = 0;
+			}
+			if (!isset($postData['recebePelosSubsetores'])) {
+				$postData['recebePelosSubsetores'] = 0;
+			}
+			$protocolo->alterar($postData);
+			$this->setSuccessMessage("Protocolo alterado com sucesso");
+		}
+
+		$this->view->protocolo = $protocolo;
+		$this->view->id = $protocolo->getId();
+	}
+
+	private function _getIdFromUrl()
+	{
+		$id = $this->getRequest()->getParam('id');
+		return $id;
+	}
 }
--- trunk/library/spu/daos/ProtocoloDao.php
+++ trunk/library/spu/daos/ProtocoloDao.php
@@ -4,40 +4,56 @@ class ProtocoloDao extends BaseDao
 {
 	private $_protocoloBaseUrl = 'spu/protocolo';
 	private $_protocoloTicketUrl = 'ticket';
-	
+
 	public function getProtocolo($nodeUuid)
 	{
 		$url = $this->getBaseUrl() . "/" . $this->_protocoloBaseUrl . "/get/$nodeUuid";
-        $url = $this->addAlfTicketUrl($url);
-        
-        $curlObj = new CurlClient();
-        $resultJson = $curlObj->doGetRequest($url);
-        $result = json_decode($resultJson, true);
-        
-        return $result['Protocolo'][0];		
+		$url = $this->addAlfTicketUrl($url);
+
+		$curlObj = new CurlClient();
+		$resultJson = $curlObj->doGetRequest($url);
+		$result = json_decode($resultJson, true);
+
+		return $result['Protocolo'][0];
 	}
-	
+
 	public function getProtocolos()
 	{
-	    $url = $this->getBaseUrl() . "/" . $this->_protocoloBaseUrl .  "/listar";
-        $url = $this->addAlfTicketUrl($url);
-        
-        $curlObj = new CurlClient();
-        $resultJson = $curlObj->doGetRequest($url);
-        $result = json_decode($resultJson, true);
-        
-        return $result['Protocolos'][0];
+		$url = $this->getBaseUrl() . "/" . $this->_protocoloBaseUrl .  "/listar";
+		$url = $this->addAlfTicketUrl($url);
+
+		$curlObj = new CurlClient();
+		$resultJson = $curlObj->doGetRequest($url);
+		$result = json_decode($resultJson, true);
+
+		return $result['Protocolos'][0];
 	}
-	
+
 	public function getTodosProtocolos()
 	{
 		$url = $this->getBaseUrl() . "/" . $this->_protocoloBaseUrl . "/listarTodos";
-        $url = $this->addAlfTicketUrl($url);
-        
-        $curlObj = new CurlClient();
-        $resultJson = $curlObj->doGetRequest($url);
-        $result = json_decode($resultJson, true);
-        
-        return $result['Protocolos'][0];
+		$url = $this->addAlfTicketUrl($url);
+
+		$curlObj = new CurlClient();
+		$resultJson = $curlObj->doGetRequest($url);
+		$result = json_decode($resultJson, true);
+
+		return $result['Protocolos'][0];
+	}
+
+	public function alterar($id, $postData)
+	{
+		$url = $this->getBaseUrl() . "/" . $this->_protocoloBaseUrl . "/editar/$id";
+		$url = $this->addAlfTicketUrl($url);
+
+		$curlObj = new CurlClient();
+
+		$result = $curlObj->doPostRequest($url, $postData);
+
+		if ($this->isAlfrescoError($result)) {
+			throw new Exception($this->getAlfrescoErrorMessage($result));
+		}
+
+		return $result['Protocolo'][0];
 	}
 }
