cb64b74fd057bdb2798fbb4f1cc8602eeee8033d
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-12-21 18:46:17 +0000

    remoção das linhas que carregavam o jquery.form.js e plupload

--- trunk/application/Bootstrap.php
+++ trunk/application/Bootstrap.php
@@ -84,10 +84,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->headScript()->appendFile($pluginsPath . 'jquery.datePicker.js');
         $view->headScript()->appendFile($pluginsPath . 'facebox.js');
         $view->headScript()->appendFile($pluginsPath . 'tableRowCheckboxToggle.js');
-        $view->headScript()->appendFile($pluginsPath . 'plupload.full.min.js');
         $view->headScript()->appendFile($pluginsPath . '/dataTables/jquery.dataTables.min.js');
         $view->headScript()->appendFile($pluginsPath . '/dataTables/textOnlyPagination.js');
-        $view->headScript()->appendFile($pluginsPath . 'jquery.form.js');
         $view->headScript()->appendFile($jsPath . 'funcoes.js');
     }
     
