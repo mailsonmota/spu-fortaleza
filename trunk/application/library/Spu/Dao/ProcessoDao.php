f85cdaadfbc363348d2628fd241ae27fae0c11ca
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2010-12-29 18:36:31 +0000

    gerenciamento de arquivos está sendo migrado para entity Arquivos e derivados

--- trunk/application/library/Spu/Dao/ProcessoDao.php
+++ trunk/application/library/Spu/Dao/ProcessoDao.php
@@ -100,28 +100,6 @@ class ProcessoDao extends BaseDao
         return $result['Processo'][0];
     }
     
-    /*
-     * Estrutura de $data
-     *   - $data['processoId']
-     *   - $data['fileContent']
-     */
-    public function uploadArquivo($postData)
-    {
-        // TODO Revisar web script "uploadarquivo"
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/uploadarquivo";
-        $url = $this->addAlfTicketUrl($url);
-        
-        $curlObj = new CurlClient();
-        
-        $result = $curlObj->doPostRequest($url, $postData, 'formdata');
-        
-        if ($this->isAlfrescoError($result)) {
-            throw new Exception($this->getAlfrescoErrorMessage($result));
-        }
-        
-        return $result;
-    }
-    
     public function tramitar($postData)
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/tramitar";
@@ -306,21 +284,5 @@ class ProcessoDao extends BaseDao
         }
         
         return $result['Processos'][0];
-    }
-    
-    public function getArquivos($nodeUuid)
-    {
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/arquivos/get/$nodeUuid";
-        $url = $this->addAlfTicketUrl($url);
-        
-        $curlObj = new CurlClient();
-        $resultJson = $curlObj->doGetRequest($url);
-        $result = json_decode($resultJson, true);
-        
-        if ($this->isAlfrescoError($result)) {
-            throw new Exception($this->getAlfrescoErrorMessage($result));
-        }
-        
-        return $result;
     }
 }
--- trunk/application/library/Spu/Entity/Arquivo.php
+++ trunk/application/library/Spu/Entity/Arquivo.php
@@ -1,6 +1,6 @@
 <?php
 require_once('BaseEntity.php');
-
+Loader::loadDao('ArquivoDao');
 class Arquivo extends BaseEntity
 {
     protected $_nome;
@@ -35,5 +35,27 @@ class Arquivo extends BaseEntity
     public function setDownloadUrl($value)
     {
         $this->_downloadUrl = $value;
+    }
+    
+    protected function _getDao()
+    {
+        return new ArquivoDao($this->_getTicket());
+    }
+    
+    public function getArquivos($Uuid)
+    {
+        $dao = $this->_getDao();
+        $arquivos = $dao->getArquivos($Uuid);
+        
+        $arquivosReturn = Array();
+        foreach ($arquivos as $arquivo) {
+             $arquivoTmp = new Arquivo();
+             $arquivoTmp->setNome($arquivo['nome']);
+             //$arquivoTmp->setDownloadUrl($dao->getBaseUrl() . $arquivo['download']);
+             $arquivoTmp->setDownloadUrl("http://172.30.116.21:8080/alfresco/service/api/node/workspace/SpacesStore/" . $arquivo['id'] . "/content/" . $arquivo['nome'] . "?alf_ticket=" . $this->_getTicket());
+             $arquivosReturn[] = $arquivoTmp;
+        }
+        
+        return $arquivosReturn;
     }
 }
