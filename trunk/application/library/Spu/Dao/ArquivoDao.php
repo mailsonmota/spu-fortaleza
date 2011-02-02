790ffab0534deb123ab469e9ee6eed842ab547c4
thiago <thiago@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-02-02 18:02:27 +0000

    modificando Dao para resgatar formulario pelo id do assunto

--- trunk/application/library/Spu/Dao/ArquivoDao.php
+++ trunk/application/library/Spu/Dao/ArquivoDao.php
@@ -2,68 +2,68 @@
 require_once('BaseDao.php');
 class ArquivoDao extends BaseDao
 {
-    private $_processoBaseUrl = 'spu/processo';
-    
-    public function getArquivos($nodeUuid)
-    {
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/arquivos/get/$nodeUuid";
-        $url = $this->addAlfTicketUrl($url);
-        
-        $curlObj = new CurlClient();
-        $resultJson = $curlObj->doGetRequest($url);
-        $result = json_decode($resultJson, true);
-        
-        if ($this->isAlfrescoError($result)) {
-            throw new Exception($this->getAlfrescoErrorMessage($result));
-        }
-        
-        return $result;
-    }
+	private $_processoBaseUrl = 'spu/processo';
 
-    /**
-     * Estrutura do $postData
-     *   $postData['processoId']
-     *   $postData['fileContent']
-     */
-    public function uploadArquivo($postData)
-    {
-        // TODO Revisar web script "uploadarquivo"
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/uploadarquivo";
-        $url = $this->addAlfTicketUrl($url);
-        
-        $curlObj = new CurlClient();
-        
-        $result = $curlObj->doPostRequest($url, $postData, 'formdata');
-        
-        if ($this->isAlfrescoError($result)) {
-            throw new Exception($this->getAlfrescoErrorMessage($result));
-        }
-        
-        return $result;
-    }
+	public function getArquivos($nodeUuid)
+	{
+		$url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/arquivos/get/$nodeUuid";
+		$url = $this->addAlfTicketUrl($url);
 
-    public function getArquivoDownloadUrl($arquivoHash)
-    {
-        $url = $this->getBaseUrl() . "/api/node/workspace/SpacesStore/"
-             . $arquivoHash['id'] . "/content/" . $arquivoHash['nome'];
-        $url = $this->addAlfTicketUrl($url);
-        return $url;
-    }
+		$curlObj = new CurlClient();
+		$resultJson = $curlObj->doGetRequest($url);
+		$result = json_decode($resultJson, true);
 
-    /**
-     *   Estrutura do $getData
-     *   $getData['id']
-     *   $getData['nome']
-     */
-    public function getContentFromUrl($getData)
-    {
-        $url = $this->getArquivoDownloadUrl($getData);
-        $curlObj = new CurlClient();
-        $result = $curlObj->doGetRequest($url);
-        /*if ($this->isAlfrescoError($result)) {
-            throw new Exception($this->getAlfrescoErrorMessage($result));
-        }*/
-        
-        return $result;
-    }
+		if ($this->isAlfrescoError($result)) {
+			throw new Exception($this->getAlfrescoErrorMessage($result));
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Estrutura do $postData
+	 *   $postData['processoId']
+	 *   $postData['fileContent']
+	 */
+	public function uploadArquivo($postData)
+	{
+		// TODO Revisar web script "uploadarquivo"
+		$url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/uploadarquivo";
+		$url = $this->addAlfTicketUrl($url);
+
+		$curlObj = new CurlClient();
+
+		$result = $curlObj->doPostRequest($url, $postData, 'formdata');
+
+		if ($this->isAlfrescoError($result)) {
+			throw new Exception($this->getAlfrescoErrorMessage($result));
+		}
+
+		return $result;
+	}
+
+	public function getArquivoDownloadUrl($arquivoHash)
+	{
+		$url = $this->getBaseUrl() . "/spu/formulario/get/assunto/"
+		. $arquivoHash['id'];
+		$url = $this->addAlfTicketUrl($url);
+		return $url;
+	}
+
+	/**
+	 *   Estrutura do $getData
+	 *   $getData['id']
+	 *   $getData['nome']
+	 */
+	public function getContentFromUrl($getData)
+	{
+		$url = $this->getArquivoDownloadUrl($getData);
+		$curlObj = new CurlClient();
+		$result = $curlObj->doGetRequest($url);
+		/*if ($this->isAlfrescoError($result)) {
+		 throw new Exception($this->getAlfrescoErrorMessage($result));
+		 }*/
+
+		return $result;
+	}
 }
