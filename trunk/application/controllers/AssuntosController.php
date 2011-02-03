5ea8a6e605255449bf95f561ed85becf0437f454
thiago <thiago@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-02-03 15:38:04 +0000

    adicionando tela com formulario

--- trunk/application/controllers/AssuntosController.php
+++ trunk/application/controllers/AssuntosController.php
@@ -2,28 +2,41 @@
 Loader::loadEntity('Assunto');
 class AssuntosController extends BaseController
 {
-    public function indexAction()
-    {
-    	$assunto = new Assunto($this->getTicket());
-        $listaAssuntos = $assunto->listar();
-        $this->view->lista = $listaAssuntos;
-    }
-    
-    public function editarAction()
-    {
-        $id = $this->_getIdFromUrl();
-        
-        $assunto = new Assunto($this->getTicket());
-        $assunto->carregarPeloId($id);
-        
-        $this->view->assunto = $assunto;
-        $this->view->id = $assunto->getId();
-        $this->view->isEdit = true;
-    }
-    
-    private function _getIdFromUrl()
-    {
-        $id = $this->getRequest()->getParam('id');
-        return $id;
-    }
+	public function indexAction()
+	{
+		$assunto = new Assunto($this->getTicket());
+		$listaAssuntos = $assunto->listar();
+		$this->view->lista = $listaAssuntos;
+	}
+
+	public function editarAction()
+	{
+		$id = $this->_getIdFromUrl();
+
+		$assunto = new Assunto($this->getTicket());
+		$assunto->carregarPeloId($id);
+
+		$this->view->assunto = $assunto;
+		$this->view->id = $assunto->getId();
+		$this->view->isEdit = true;
+	}
+
+	private function _getIdFromUrl()
+	{
+		$id = $this->getRequest()->getParam('id');
+		return $id;
+	}
+	
+	public function formularioAction()
+	{
+		$id = $this->_getIdFromUrl();
+
+		$assunto = new Assunto($this->getTicket());
+		$assunto->carregarPeloId($id);
+
+		$this->view->assunto = $assunto;
+		$this->view->id = $assunto->getId();
+		$this->view->isEdit = true;
+		$this->view->result = $assunto->getFormularioXsd();
+	}
 }
