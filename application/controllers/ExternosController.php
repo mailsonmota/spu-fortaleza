0eedc7d573e451bf69b111d4865b5db1004a5f56
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-02-14 19:15:05 +0000

    adequação aos padrões de código Zend

--- application/controllers/ExternosController.php
+++ application/controllers/ExternosController.php
@@ -2,21 +2,21 @@
 require_once('BaseTramitacaoController.php');
 class ExternosController extends BaseTramitacaoController
 {
-	public function indexAction()
+    public function indexAction()
     {
-    	$processoService = new ProcessoService($this->getTicket());
-    	
-    	if ($this->getRequest()->isPost()) {
-    		try {
-    			$processosSelecionados = $this->getRequest()->getParam('processos');
-    			$session = new Zend_Session_Namespace('encaminhar');
-        		$session->processos = $processosSelecionados;
-        		$this->_redirectEncaminhar();
-    		} catch (Exception $e) {
-	    		$this->setMessageForTheView($e->getMessage(), 'error');
-	    	}
-    	}
-    	
+        $processoService = new ProcessoService($this->getTicket());
+        
+        if ($this->getRequest()->isPost()) {
+            try {
+                $processosSelecionados = $this->getRequest()->getParam('processos');
+                $session = new Zend_Session_Namespace('encaminhar');
+                $session->processos = $processosSelecionados;
+                $this->_redirectEncaminhar();
+            } catch (Exception $e) {
+                $this->setMessageForTheView($e->getMessage(), 'error');
+            }
+        }
+        
         $this->view->lista = $processoService->getCaixaExternos();
     }
 }
--- application/controllers/ProtocolosController.php
+++ application/controllers/ProtocolosController.php
@@ -2,22 +2,23 @@
 Loader::loadService('ProtocoloService');
 class ProtocolosController extends BaseController
 {
-	public function indexAction() {}
+    public function indexAction() {
+    }
 
-	public function editarAction()
-	{
-		$id = $this->_getIdFromUrl();
+    public function editarAction()
+    {
+        $id = $this->_getIdFromUrl();
 
-		$protocoloService = new ProtocoloService($this->getTicket());
-		$protocolo = $protocoloService->getProtocolo($id);
-		
-		$this->view->protocolo = $protocolo;
-		$this->view->id = $protocolo->getId();
-	}
+        $protocoloService = new ProtocoloService($this->getTicket());
+        $protocolo = $protocoloService->getProtocolo($id);
+        
+        $this->view->protocolo = $protocolo;
+        $this->view->id = $protocolo->getId();
+    }
 
-	private function _getIdFromUrl()
-	{
-		$id = $this->getRequest()->getParam('id');
-		return $id;
-	}
+    private function _getIdFromUrl()
+    {
+        $id = $this->getRequest()->getParam('id');
+        return $id;
+    }
 }
