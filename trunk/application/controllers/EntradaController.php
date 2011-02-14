c6a7df3dca32aa93a8cdfe17e9fa6a11efcf8ccf
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-02-14 19:15:05 +0000

    adequação aos padrões de código Zend

--- trunk/application/controllers/EntradaController.php
+++ trunk/application/controllers/EntradaController.php
@@ -2,21 +2,22 @@
 require_once('BaseTramitacaoController.php');
 class EntradaController extends BaseTramitacaoController
 {
-	public function indexAction() {}
+    public function indexAction() {
+    }
     
     public function receberAction()
     {
-    	try {
-    		if (!$this->getRequest()->isPost() OR !$this->getRequest()->getParam('processos')) {
-    			throw new Exception("Por favor, selecione pelo menos um processo para receber.");
-    		}
-    		$processoService = new ProcessoService($this->getTicket());
-    		$processoService->receberVarios($this->getRequest()->getPost());
-    		$this->setSuccessMessage('Processos recebidos com sucesso.');
-    	} catch (Exception $e) {
-    		$this->setErrorMessage($e->getMessage());
-    		$this->_redirectEntrada();
-    	}
-    	$this->_redirectEmAnalise();
+        try {
+            if (!$this->getRequest()->isPost() OR !$this->getRequest()->getParam('processos')) {
+                throw new Exception("Por favor, selecione pelo menos um processo para receber.");
+            }
+            $processoService = new ProcessoService($this->getTicket());
+            $processoService->receberVarios($this->getRequest()->getPost());
+            $this->setSuccessMessage('Processos recebidos com sucesso.');
+        } catch (Exception $e) {
+            $this->setErrorMessage($e->getMessage());
+            $this->_redirectEntrada();
+        }
+        $this->_redirectEmAnalise();
     }
 }
