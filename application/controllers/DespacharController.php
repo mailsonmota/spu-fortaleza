cbb5e0eaf586aeb097882b3c3fbc32a785a7f30d
gil <gil@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-02-14 17:41:25 +0000

    despacho com arquivos

--- application/controllers/DespacharController.php
+++ application/controllers/DespacharController.php
@@ -4,15 +4,42 @@ class DespacharController extends BaseTramitacaoController
 {
 	public function indexAction()
     {
-    	if ($this->getRequest()->isPost()) {
-    		try {
-    			$processoService = new ProcessoService($this->getTicket());
-	    		$processoService->comentarVarios($this->getRequest()->getPost());
-	    		$this->setSuccessMessage('Despachos criados com sucesso.');
-	    		$this->_redirectEmAnalise();
-			} catch (Exception $e) {
-	    		$this->setMessageForTheView($e->getMessage(), 'error');
-	    	}
+        $sessionDespachar = new Zend_Session_Namespace('despachar');
+        
+        if ($this->getRequest()->isPost()) {
+            if (!empty($_FILES)) {
+                $fileTmp = $this->_uploadFilePathConverter($_FILES['fileToUpload']['name'],
+                                                           $_FILES['fileToUpload']['tmp_name']);
+                foreach ($sessionDespachar->filesToUpload as $fileToUpload) {
+                    if ($fileToUpload == $fileTmp) {
+                        $this->setErrorMessage('Este arquivo já está adicionado a lista de arquivos a ser submetida.');
+                        $this->_redirectIndex();
+                    }
+                }
+                $sessionDespachar->filesToUpload[] = $fileTmp;
+            } else {
+                try {
+                    $postData = $this->getRequest()->getPost();
+                    
+        			$processoService = new ProcessoService($this->getTicket());
+    	    		$processoService->comentarVarios($this->getRequest()->getPost());
+    	    		
+    	    		$arquivoService = new ArquivoService($this->getTicket());
+    	    		
+    	    		foreach($postData['processos'] as $processoId) {
+    	    		    $postData['destNodeUuid'] = $processoId;
+        	    		foreach ($sessionDespachar->filesToUpload as $fileToUpload) {
+        	    		    $postData['fileToUpload'] = $fileToUpload;
+        	    		    $arquivoService->uploadArquivo($postData);
+        	    		}
+    	    		}
+    	    		
+    	    		$this->setSuccessMessage('Despachos criados com sucesso.');
+    	    		$this->_redirectEmAnalise();
+    			} catch (Exception $e) {
+    	    		$this->setMessageForTheView($e->getMessage(), 'error');
+    	    	}
+            }
     	}
     	
     	$processos = array();
@@ -27,5 +54,21 @@ class DespacharController extends BaseTramitacaoController
     	}
     	
         $this->view->processos = $processos;
+        $this->view->filesToUpload = $sessionDespachar->filesToUpload;
+    }
+    
+    protected function removerarquivoAction()
+    {
+        $numero = $this->getRequest()->getParam('removerarquivo');
+        $sessionDespachar = new Zend_Session_Namespace('despachar');
+        unset($sessionDespachar->filesToUpload[$numero]);
+        $sessionDespachar->filesToUpload = array_values($sessionDespachar->filesToUpload);
+        $this->setSuccessMessage('Arquivo removido da lista de arquivo a ser submetida.');
+        $this->_redirectIndex();
+    }
+    
+    protected function _redirectIndex()
+    {
+        $this->_helper->redirector('index', 'despachar', 'default');
     }
 }
