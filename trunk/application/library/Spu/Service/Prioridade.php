56e395631bfbd71f18e7f58d0438210f20018448
brunofcavalcante@gmail.com <brunofcavalcante@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-09-20 18:07:20 +0000

    code comments

--- trunk/application/library/Spu/Service/Prioridade.php
+++ trunk/application/library/Spu/Service/Prioridade.php
@@ -1,8 +1,24 @@
 <?php
+/**
+ * Classe para acessar os serviços de Prioridade dos Processos do SPU
+ * 
+ * @author Bruno Cavalcante <brunofcavalcante@gmail.com>
+ * @package SPU
+ * @see Spu_Service_Abstract
+ */
 class Spu_Service_Prioridade extends Spu_Service_Abstract
 {
+	/**
+	 * URL Base dos serviços (a ser acrescentada à url dos serviços do Alfresco)
+	 * @var string
+	 */
     private $_baseUrl = 'spu/processo';
     
+    /**
+     * Retorna todas as opções de Prioridade do SPU
+     * 
+     * @return Spu_Entity_Classification_Prioridade[]
+     */
     public function fetchAll()
     {
         $url = $this->getBaseUrl() . "/" . $this->_baseUrl . "/prioridades/listar";
@@ -11,6 +27,12 @@ class Spu_Service_Prioridade extends Spu_Service_Abstract
         return $this->_loadManyFromHash($result['Prioridades'][0]);
     }
     
+    /**
+     * Carrega uma Prioridade à partir de um hash
+     * 
+     * @param array $hash
+     * @return Spu_Entity_Classification_Prioridade
+     */
     public function loadFromHash($hash)
     {
         $prioridade = new Spu_Entity_Classification_Prioridade();
@@ -22,6 +44,12 @@ class Spu_Service_Prioridade extends Spu_Service_Abstract
         return $prioridade;
     }
     
+    /**
+     * Carrega várias Prioridades à partir de um hash
+     * 
+     * @param array $hash
+     * @return Spu_Entity_Classification_Prioridade[]
+     */
     protected function _loadManyFromHash($hash)
     {
         $prioridades = array();
