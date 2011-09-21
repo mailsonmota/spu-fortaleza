cedb3b010e7c6c3ee483a99296114ec25c2c1092
brunofcavalcante@gmail.com <brunofcavalcante@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-09-21 17:12:51 +0000

    refactoring

--- trunk/application/library/Spu/Service/TipoTramitacao.php
+++ trunk/application/library/Spu/Service/TipoTramitacao.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Classe para acessar os serviços de Tipo de Manifestante dos Processos do SPU
+ *
+ * @author Bruno Cavalcante <brunofcavalcante@gmail.com>
+ * @package SPU
+ * @see Spu_Service_Abstract
+ */
 class Spu_Service_TipoTramitacao extends Spu_Service_Abstract
 {
+	/**
+	 * URL Base dos serviços (a ser acrescentada à url dos serviços do Alfresco)
+	 * @var string
+	 */
     private $_baseUrl = 'spu/tiposprocesso';
-    private $_ticketUrl = 'ticket';
     
+    /**
+     * Retorna todas as opções de tipo de abrangência
+     *
+     * @return Spu_Entity_Classification_TipoTramitacao[]
+     */
     public function fetchAll()
     {
         $url = $this->getBaseUrl() . "/" . $this->_baseUrl . "/tramitacoes/listar";
@@ -12,6 +27,12 @@ class Spu_Service_TipoTramitacao extends Spu_Service_Abstract
         return $this->_loadManyFromHash($result['Tramitacoes'][0]);
     }
     
+    /**
+     * Carrega o tipo de tramitação através de um hash
+     *
+     * @param array $hash
+     * @return Spu_Entity_Classification_TipoTramitacao
+     */
     public function loadFromHash($hash)
     {
         $tipoTramitacao = new Spu_Entity_Classification_TipoTramitacao();
@@ -23,6 +44,12 @@ class Spu_Service_TipoTramitacao extends Spu_Service_Abstract
         return $tipoTramitacao;
     }
     
+    /**
+     * Carrega vários tipos de tramitação através de um hash
+     *
+     * @param array $hash
+     * @return Spu_Entity_Classification_TipoTramitacao[]
+     */
     protected function _loadManyFromHash($hash)
     {
         $tiposTramitacao = array();
