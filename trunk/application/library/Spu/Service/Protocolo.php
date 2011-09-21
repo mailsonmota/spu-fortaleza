bf7d65b825960e7f2286185f1d8df7cff9aee944
gilmagno@gmail.com <gilmagno@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-09-21 18:07:40 +0000

    Refatoração.

--- trunk/application/library/Spu/Service/Protocolo.php
+++ trunk/application/library/Spu/Service/Protocolo.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * Classe para acessar os serviços de Protocolo do SPU
- * 
+ *
  * @author Bruno Cavalcante <brunofcavalcante@gmail.com>
  * @package SPU
  * @see Spu_Service_Abstract
  */
 class Spu_Service_Protocolo extends Spu_Service_Abstract
 {
-	/**
-	 * URL Base dos serviços (a ser acrescentada à url dos serviços do Alfresco)
-	 * @var string
-	 */
+    /**
+     * URL Base dos serviços (a ser acrescentada à url dos serviços do Alfresco)
+     * @var string
+     */
     private $_protocoloBaseUrl = 'spu/protocolo';
-    
+
     /**
      * Retorna o protocolo pelo seu id
-     * 
+     *
      * @param string $nodeUuid
      * @return Spu_Entity_Protocolo
      */
@@ -30,7 +30,7 @@ class Spu_Service_Protocolo extends Spu_Service_Abstract
 
     /**
      * Retorna todos os protocolos do usuário logado
-     * 
+     *
      * @return Spu_Entity_Protocolo[]
      */
     public function getProtocolos()
@@ -43,22 +43,22 @@ class Spu_Service_Protocolo extends Spu_Service_Abstract
 
     /**
      * Altera os dados de um protocolo
-     * 
+     *
      * @param string $id
-     * @param array $postData array com os dados do protocolo 
+     * @param array $postData array com os dados do protocolo
      * @return Spu_Entity_Protocolo
      */
     public function alterar($id, $postData)
     {
         $url = $this->getBaseUrl() . "/" . $this->_protocoloBaseUrl . "/editar/$id";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $this->loadFromHash($result['Protocolo'][0]);
     }
-    
+
     /**
      * Retorna todos os protocolos do SPU, paginados
-     * 
+     *
      * @param integer $offset
      * @param integer $pageSize
      * @param string $filter
@@ -68,15 +68,15 @@ class Spu_Service_Protocolo extends Spu_Service_Abstract
     {
         $filter = urlencode($filter);
         $url = "{$this->getBaseUrl()}/{$this->_protocoloBaseUrl}/listarTodosPaginado/$offset/$pageSize/?s=$filter";
-        
+
         $result = $this->_doAuthenticatedGetRequest($url);
-        
+
         return $this->_loadManyFromHash($result['Protocolos'][0]);
     }
-    
+
     /**
      * Retorna os protocolos (paginados) que podem ser destino, à partir do protocolo de origem e o tipo de processo
-     * 
+     *
      * @param string $protocoloOrigemId
      * @param string $tipoProcessoId
      * @param string $filter
@@ -86,37 +86,37 @@ class Spu_Service_Protocolo extends Spu_Service_Abstract
      */
     public function getProtocolosDestino($protocoloOrigemId, $tipoProcessoId, $filter, $offset, $pageSize)
     {
-        $url = "{$this->getBaseUrl()}/{$this->_protocoloBaseUrl}/listardestinos?protocoloRaizId={$protocoloOrigemId}";
-        $url .= "&tipoProcessoId={$tipoProcessoId}&filter={$filter}&offset={$offset}&pageSize={$pageSize}";
-        
+        $url = "{$this->getBaseUrl()}/{$this->_protocoloBaseUrl}/listardestinos?protocoloRaizId={$protocoloOrigemId}"
+             . "&tipoProcessoId={$tipoProcessoId}&filter={$filter}&offset={$offset}&pageSize={$pageSize}";
+
         $result = $this->_doAuthenticatedGetRequest($url);
-                
+
         return $this->_loadManyFromHash($result['Protocolos'][0]);
     }
 
     /**
      * Carrega o protocolo através de um hash
-     * 
+     *
      * @param array $hash
      * @return Spu_Entity_Protocolo
      */
     public function loadFromHash($hash)
     {
         $protocolo = new Spu_Entity_Protocolo();
-        
+
         $protocolo->setNodeRef($this->_getHashValue($hash, 'noderef'));
         $protocolo->setNome($this->_getHashValue($hash, 'nome'));
         $protocolo->setParent($this->_loadParentFromHash($this->_getHashValue($hash, 'parentId')));
         $protocolo->setDescricao($this->_getHashValue($hash, 'descricao'));
         $protocolo->setNivel($this->_getHashValue($hash, 'nivel'));
         $protocolo->setPath($this->_getHashValue($hash, 'path'));
-        
+
         return $protocolo;
     }
-    
+
     /**
      * Carrega somente os dados básicos do protocolo pai
-     * 
+     *
      * @param string $id
      * @return Spu_Entity_Protocolo
      */
@@ -124,13 +124,13 @@ class Spu_Service_Protocolo extends Spu_Service_Abstract
     {
         $parent = new Spu_Entity_Protocolo();
         $parent->setNodeRef($id);
-        
+
         return $parent;
     }
-    
+
     /**
      * Carrega vários protocolos através de um hash
-     * 
+     *
      * @param array $hash
      * @return Spu_Entity_Protocolo[]
      */
@@ -138,10 +138,10 @@ class Spu_Service_Protocolo extends Spu_Service_Abstract
     {
         $protocolos = array();
         if ($hash) {
-	        foreach ($hash as $hashProtocolo) {
-	            $hashProtocolo = array_pop($hashProtocolo);
-	            $protocolos[] = $this->loadFromHash($hashProtocolo);
-	        }
+            foreach ($hash as $hashProtocolo) {
+                $hashProtocolo = array_pop($hashProtocolo);
+                $protocolos[] = $this->loadFromHash($hashProtocolo);
+            }
         }
 
         return $protocolos;
