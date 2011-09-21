591cded567e274a5f8c2a7068e5f12930d2b5614
brunofcavalcante <brunofcavalcante@gmail.com>
2011-09-21 16:43:56 +0000

    refactoring

--- application/library/Spu/Service/TipoAbrangencia.php
+++ application/library/Spu/Service/TipoAbrangencia.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Classe para acessar os serviços de Tipo de Abrangência dos Processos do SPU
+ *
+ * @author Bruno Cavalcante <brunofcavalcante@gmail.com>
+ * @package SPU
+ * @see Spu_Service_Abstract
+ */
 class Spu_Service_TipoAbrangencia extends Spu_Service_Abstract
 {
+	/**
+	 * URL Base dos serviços (a ser acrescentada à url dos serviços do Alfresco)
+	 * @var string
+	 */
     private $_baseUrl = 'spu/tiposprocesso';
-    private $_ticketUrl = 'ticket';
     
+    /**
+     * Retorna todas as opções de tipo de abrangência
+     * 
+     * @return Spu_Entity_Classification_TipoAbrangencia[]
+     */
     public function fetchAll()
     {
         $url = $this->getBaseUrl() . "/" . $this->_baseUrl . "/abrangencias/listar";
@@ -12,6 +27,12 @@ class Spu_Service_TipoAbrangencia extends Spu_Service_Abstract
         return $this->_loadManyFromHash($result['Abrangencias'][0]);
     }
     
+    /**
+     * Carrega o tipo de abrangência através de um hash
+     * 
+     * @param array $hash
+     * @return Spu_Entity_Classification_TipoAbrangencia
+     */
     public function loadFromHash($hash)
     {
         $tipoAbrangencia = new Spu_Entity_Classification_TipoAbrangencia();
@@ -23,6 +44,12 @@ class Spu_Service_TipoAbrangencia extends Spu_Service_Abstract
         return $tipoAbrangencia;
     }
     
+    /**
+     * Carrega vários tipos de abrangência através de um hash
+     * 
+     * @param array $hash
+     * @return Spu_Entity_Classification_TipoAbrangencia[]
+     */
     protected function _loadManyFromHash($hash)
     {
         $tiposAbrangencia = array();
