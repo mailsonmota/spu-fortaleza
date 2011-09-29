182f26ab62dac5960ff6cde10690c2b2e8677d3a
brunofcavalcante <brunofcavalcante@gmail.com>
2011-09-29 15:47:12 +0000

    consulta por assunto nas caixas de entrada, analise e externos

--- application/controllers/AnaliseController.php
+++ application/controllers/AnaliseController.php
@@ -49,13 +49,17 @@ class AnaliseController extends BaseTramitacaoController
         }
         
         $this->view->q = urldecode($this->_getParam('q'));
+        $this->view->tipoProcessoId = urldecode($this->_getParam('tipo-processo'));
+        $this->view->assuntoId = urldecode($this->_getParam('assunto'));
+        $this->view->tiposProcesso = $this->_getListaTiposProcesso();
          
         $service = new Spu_Service_Tramitacao($this->getTicket());
         $this->view->paginator = $this->_helper->paginator()->paginate(
 	        $service->getCaixaAnalise(
 		        $this->_helper->paginator()->getOffset(),
 		        $this->_helper->paginator()->getPageSize(),
-		        $this->view->q
+		        $this->view->q, 
+		        $this->view->assuntoId
 	        )
         );
     }
