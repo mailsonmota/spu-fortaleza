37a184f02a4a8be3f72e11cde6aa292be389a95f
brunofcavalcante@gmail.com <brunofcavalcante@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-10-04 17:44:08 +0000

    refactoring

--- trunk/application/controllers/IndexController.php
+++ trunk/application/controllers/IndexController.php
@@ -7,5 +7,10 @@ class IndexController extends BaseController
             $this->setMessageForTheView('Você não tem acesso à nenhum setor de protocolo, portanto
                                   não poderá abrir ou tramitar processos.');
         }
+        
+        $srv = new Alfresco_Rest_SpacesStore(Spu_Service_Abstract::getAlfrescoUrl(), $this->getTicket());
+        $result = $srv->getRootFolders();
+        
+        echo '<pre>'; var_dump($result); exit;
     }
 }
