8a41c27f282b98fb247b5217195d37b7cb85eb6b
gilmagno <gilmagno@gmail.com>
2011-10-05 22:28:45 +0000

    Move alguns web scripts de dirs.

--- alfresco/Web Scripts Extensions/org/spu/cadastro/protocoloslistarfilhos/protocoloslistarfilhos.get.js
+++
@@ -1,7 +0,0 @@
-<import resource="/Company Home/Data Dictionary/Scripts/SPU/protocolo.js">
-
-var url = url.extension.split('/')
-var protocoloRaizId = url[0]
-
-model.protocolos = getProtocolosUnder(protocoloRaizId)
-
