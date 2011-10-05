ddef29246640dae21e27237b74e94fb0febf15c1
brunofcavalcante@gmail.com <brunofcavalcante@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-10-05 17:03:52 +0000

    refactoring na api de alfresco

--- trunk/application/controllers/IndexController.php
+++ trunk/application/controllers/IndexController.php
@@ -7,5 +7,15 @@ class IndexController extends BaseController
             $this->setMessageForTheView('Você não tem acesso à nenhum setor de protocolo, portanto
                                   não poderá abrir ou tramitar processos.');
         }
+        
+        $srv = new Alfresco_Rest_SpacesStore(Spu_Service_Abstract::getAlfrescoUrl(), $this->getTicket());
+    
+        $rootFolders = $srv->getRootFolders();
+        
+        $childrenNodes = $srv->getChildren($rootFolders[4]->id);
+        
+        $protocolos = $srv->getChildren($childrenNodes[0]->id);
+        
+        echo '<pre>'; var_dump($protocolos);
     }
 }
