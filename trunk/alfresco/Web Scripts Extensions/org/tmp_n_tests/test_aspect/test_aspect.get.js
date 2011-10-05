4269e0c5dbc8ffccb9515b0492d8e2e58d83e69b
gilmagno@gmail.com <gilmagno@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-10-05 14:21:18 +0000

    Atualiza backup de arquivos do Alfresco.

--- trunk/alfresco/Web Scripts Extensions/org/tmp_n_tests/test_aspect/test_aspect.get.js
+++
@@ -1,6 +0,0 @@
-<import resource="/Company Home/Data Dictionary/Scripts/SPU/processo.js">
-
-var processo = getNode('f6d2d872-cc96-4de7-bcbb-0105c679a398')
-var varToEval = processo.properties['spu:folhas.Volumes'];
-var arrayQwe = eval('(' + varToEval + ')');
-throw arrayQwe[1]['nome']
