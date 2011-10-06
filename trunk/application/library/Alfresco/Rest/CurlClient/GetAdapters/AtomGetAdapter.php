501fe163b1771c95dc2f08c2e92054c7d1d62f65
gilmagno@gmail.com <gilmagno@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-10-06 15:10:44 +0000

    Deleta Api do Alfresco.

--- trunk/application/library/Alfresco/Rest/CurlClient/GetAdapters/AtomGetAdapter.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Alfresco_Rest_CurlClient_GetAdapters_AtomGetAdapter implements Alfresco_Rest_CurlClient_GetAdapters_Interface
-{
-    public function decode($data, $assoc = false) {
-        $xml = DOMDocument::loadXML($data); 
-        return $xml;
-    }
-}
--- trunk/application/library/Alfresco/Rest/CurlClient/PostAdapters/Interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface Alfresco_Rest_CurlClient_PostAdapters_Interface
-{
-    public function encode($data);
-    public function updateOptions($options);
-}
