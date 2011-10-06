82d94a69fcc3fa8d1b482defec2d4be5c987b5a1
gilmagno <gilmagno@gmail.com>
2011-10-06 15:10:44 +0000

    Deleta Api do Alfresco.

--- application/library/Alfresco/Rest/CurlClient/GetAdapters/Interface.php
+++
@@ -1,5 +0,0 @@
-<?php
-interface Alfresco_Rest_CurlClient_GetAdapters_Interface
-{
-    public function decode($data, $assoc);
-}
--- application/library/Alfresco/Rest/CurlClient/GetAdapters/JsonGetAdapter.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Alfresco_Rest_CurlClient_GetAdapters_JsonGetAdapter implements Alfresco_Rest_CurlClient_GetAdapters_Interface
-{
-    public function decode($data, $assoc = false) {
-        // FIXME fazer ou procurar função que valide json
-        return json_decode($data, $assoc);
-    }
-}
