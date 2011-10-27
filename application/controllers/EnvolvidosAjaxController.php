3f3e997fff5a51683b6093a07cee45448dddd4f8
igor.roben <igor.roben@gmail.com>
2011-10-27 19:23:18 +0000

    mascara input cpf cnpj envolvidos

--- application/controllers/EnvolvidosAjaxController.php
+++ application/controllers/EnvolvidosAjaxController.php
@@ -4,16 +4,15 @@ class EnvolvidosAjaxController extends BaseAuthenticatedController
     public function envolvidoAction()
     {
         $this->_helper->layout()->disableLayout();
-        
-        $this->view->manifestante = $this->_getManifestante($this->_getSearchTerm());
+        $this->view->manifestante = $this->_getManifestante(str_replace(array(".","-", "/"), "",$this->_getSearchTerm()));
     }
-    
+
     protected function _getManifestante($cpfCnpj)
     {
         $manifestanteService = new Spu_Service_Manifestante($this->getTicket());
         return $manifestanteService->getManifestante($cpfCnpj);
     }
-    
+
     protected function _getSearchTerm()
     {
         return $this->getRequest()->getParam('term', null);
