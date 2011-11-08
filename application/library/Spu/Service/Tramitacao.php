b4b950fc331c3e7a3b84a2ccb8d86d3aaa82c186
gilmagno <gilmagno@gmail.com>
2011-11-08 18:05:15 +0000

    Muda services e controllers para que a busca de processos nas caixas de entrada, saída etc. possa ser feita com múltiplos argumento no campo "Número/Envolvido".

--- application/library/Spu/Service/Tramitacao.php
+++ application/library/Spu/Service/Tramitacao.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Classe para acessar os serviços de tramitação de processo do SPU
- * 
+ *
  * @author Bruno Cavalcante <brunofcavalcante@gmail.com>
  * @package SPU
  * @see Spu_Service_Processo
@@ -10,7 +10,7 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
 {
     /**
      * Retorna os processos nas caixas de entrada do usuário
-     * 
+     *
      * @param integer $offset
      * @param integer $pageSize
      * @param string filter
@@ -24,13 +24,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
         if ($assuntoId) {
             $url .= "?assunto-id=$assuntoId";
         }
-        
+
         return $this->_loadManyFromHash($this->_getProcessosFromUrl($url));
     }
-    
+
     /**
      * Retorna os processos que foram enviados pelo usuário, e ainda não foram recebidos pelo destinatário
-     * 
+     *
      * @param integer $offset
      * @param integer $pageSize
      * @param string filter
@@ -38,18 +38,19 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
      */
     public function getCaixaSaida($offset, $pageSize, $filter, $assuntoId = null)
     {
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/saida/$offset/$pageSize/$filter";
-        
+        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl
+            . "/saida/$offset/$pageSize/" . str_replace(' ', '', $filter);
+
         if ($assuntoId) {
             $url .= "?assunto-id=$assuntoId";
         }
-        
+
         return $this->_loadManyFromHash($this->_getProcessosFromUrl($url));
     }
-    
+
     /**
      * Retorna os processos que estão nas caixas de análise do usuário
-     * 
+     *
      * @param integer $offset
      * @param integer $pageSize
      * @param string filter
@@ -57,18 +58,19 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
      */
     public function getCaixaAnalise($offset, $pageSize, $filter, $assuntoId = null)
     {
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/analise/$offset/$pageSize/$filter";
-        
+        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl
+             . "/analise/$offset/$pageSize/" . str_replace(' ', '', $filter);
+
         if ($assuntoId) {
             $url .= "?assunto-id=$assuntoId";
         }
-        
+
         return $this->_loadManyFromHash($this->_getProcessosFromUrl($url));
     }
-    
+
     /**
      * Retorna os processos que foram enviados pelo usuário e recebidos pelo destinatário
-     * 
+     *
      * @param integer $offset
      * @param integer $pageSize
      * @param string filter
@@ -76,18 +78,19 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
      */
     public function getCaixaEnviados($offset, $pageSize, $filter, $assuntoId = null)
     {
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/enviados/$offset/$pageSize/$filter";
-        
+        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl
+             . "/enviados/$offset/$pageSize/" . str_replace(' ', '', $filter);
+
         if ($assuntoId) {
             $url .= "?assunto-id=$assuntoId";
         }
-        
+
         return $this->_loadManyFromHash($this->_getProcessosFromUrl($url));
     }
-    
+
     /**
      * Retorna os processos que estão na caixa de análise do usuário, porém marcados como "Externos"
-     * 
+     *
      * @param integer $offset
      * @param integer $pageSize
      * @param string filter
@@ -95,18 +98,19 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
      */
     public function getCaixaExternos($offset = 0, $pageSize = 20, $filter = '', $assuntoId = null)
     {
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/externos/$offset/$pageSize/$filter";
-        
+        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl
+             . "/externos/$offset/$pageSize/" . str_replace(' ', '', $filter);
+
         if ($assuntoId) {
             $url .= "?assunto-id=$assuntoId";
         }
-        
+
         return $this->_loadManyFromHash($this->_getProcessosFromUrl($url));
     }
-    
+
     /**
      * Retorna os processos que estão nas caixas de arquivo do usuário
-     * 
+     *
      * @param integer $offset
      * @param integer $pageSize
      * @param string filter
@@ -114,18 +118,19 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
      */
     public function getCaixaArquivo($offset, $pageSize, $filter, $assuntoId = null)
     {
-        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/arquivo/$offset/$pageSize/$filter";
-        
+        $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl
+             . "/arquivo/$offset/$pageSize/" . str_replace(' ', '', $filter);
+
         if ($assuntoId) {
             $url .= "?assunto-id=$assuntoId";
         }
-        
+
         return $this->_loadManyFromHash($this->_getProcessosFromUrl($url));
     }
-    
+
     /**
      * Tramita um processo para um outro protocolo
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -133,13 +138,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/tramitar";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Tramita vários processos para um outro protocolo
-     * 
+     *
      * @param array $postData
      * @return array parametros podem ser conferidos no webscript
      */
@@ -147,13 +152,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/tramitarProcessos";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Recebe vários processos e os coloca na caixa de análise
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -161,13 +166,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/receber";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Marca vários processos como "Externos"
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -175,13 +180,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/tramitarExternos";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Retorna vários processos da situação de "Externos"
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -189,13 +194,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/retornarExternos";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Cancela o envio de processos nas caixas de saída do usuário e os retorna pra caixa de entrada
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -203,13 +208,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/cancelarEnvios";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Arquiva vários processos
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -217,13 +222,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/arquivar";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Reabre vários processos e os coloca de volta na caixa de análise
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -231,13 +236,13 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/reabrir";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
-    
+
     /**
      * Adiciona um novo despacho em vários processos
-     * 
+     *
      * @param array $postData parametros podem ser conferidos no webscript
      * @return array
      */
@@ -245,7 +250,7 @@ class Spu_Service_Tramitacao extends Spu_Service_Processo
     {
         $url = $this->getBaseUrl() . "/" . $this->_processoBaseUrl . "/comentar";
         $result = $this->_doAuthenticatedPostRequest($url, $postData);
-        
+
         return $result;
     }
 }
