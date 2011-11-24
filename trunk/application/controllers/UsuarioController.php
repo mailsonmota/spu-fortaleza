d862b3f7073754422466b2a68ee865885e572616
igor.roben@gmail.com <igor.roben@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-11-24 19:19:09 +0000

    finalizacao area edit password -fase final da regra de negocio

--- trunk/application/controllers/UsuarioController.php
+++ trunk/application/controllers/UsuarioController.php
@@ -14,23 +14,50 @@ class UsuarioController extends BaseController
 
         if ($data) {
             if (!$data['new'] || !$data['confirm'] || !$data['current']) {
-                $this->redirectFalha('Operação inválida! Preencha todos os campos.');
-            }
-            else if ($data['new'] != $data['confirm']) {
-                $this->redirectFalha('Falha na operação! A senha e sua confirmação não estão iguais.');
-            }
-            else if (strlen($data['new']) < 3){
-                $this->redirectFalha('Atenção, a nova senha deve conter no mínimo 3 caracteres.');
-            }
-            else {
-                $this->setMessageForTheView(print_r($data));
+                $this->_redirectFalha('Operação inválida! Preencha todos os campos.');
+            } else if ($data['new'] != $data['confirm']) {
+                $this->_redirectFalha('Falha na operação! A senha e sua confirmação não estão iguais.');
+            } else if (strlen($data['new']) < 3) {
+                $this->_redirectFalha('Atenção, a nova senha deve conter no mínimo 3 caracteres.');
+            } else {
+                if (!$this->_checkSenhaAtual($data['current'])) {
+                    $this->_redirectFalha('Atenção, a senha atual do usuário não está correta.');
+                } else {
+                    if ($this->_updateSenha($data['current'], $data['new'])) {
+                        $this->_redirectSucess('Operação realizada com sucesso!');
+                    }
+                    else {
+                        $this->_redirectFalha();
+                    }
+                }
             }
         }
     }
 
-    private function redirectFalha($str = 'Ops!!! Ocorreu um erro.')
-    {   $this->setErrorMessage($str);
+    private function _redirectFalha($str = 'Ops!!! Ocorreu um erro.')
+    {
+        $this->setErrorMessage($str);
         $this->_helper->redirector('edit');
+    }
+
+    private function _redirectSucess($str = 'Ops!!! Ocorreu um erro.')
+    {
+        $this->setSuccessMessage($str);
+        $this->_helper->redirector('edit');
+    }
+
+    private function _updateSenha($oldpw, $newpw)
+    {
+        $alfrescoPerson = new Alfresco_Rest_Person(Spu_Service_Abstract::getAlfrescoUrl(), $this->getTicket());
+
+        return $alfrescoPerson->updatePassword($this->getUser()->login, $oldpw, $newpw);
+    }
+
+    private function _checkSenhaAtual($password)
+    {
+        $alfrescoLogin = new Alfresco_Rest_Login(Spu_Service_Abstract::getAlfrescoUrl(), $this->getTicket());
+
+        return $alfrescoLogin->checkLogin($this->getUser()->login, $password);
     }
 
     protected function _getNomeProtocolosUsuario()
--- trunk/application/library/Alfresco/Rest/Login.php
+++ trunk/application/library/Alfresco/Rest/Login.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @see Alfresco_Rest_Abstract
  */
@@ -6,31 +7,32 @@ require_once 'Abstract.php';
 
 /**
  * Login/Logout service
- * 
+ *
  * @author Gil Magno <gilmagno@gmail.com>
  * @package alfresco-php-api
  */
 class Alfresco_Rest_Login extends Alfresco_Rest_Abstract
 {
+
     /**
      * URL package name for the services (this will be added to the alfresco base services url)
-     * 
+     *
      * @var string
      */
     private $_loginBaseUrl = 'login';
-    
+
     /**
      * URL param name for the ticket (this will be added to the loginBaseUrl)
-     * 
+     *
      * @var string
      */
     private $_loginTicketUrl = 'ticket';
-    
+
     /**
      * Basic authentication service
-     * 
+     *
      * POST /alfresco/service/api/login
-     * 
+     *
      * @param string $username
      * @param string $password
      * @return array array with the authentication ticket. Example: array('ticket' => 'ahq812GasLPlsmNMskneulasJsjak')
@@ -38,45 +40,68 @@ class Alfresco_Rest_Login extends Alfresco_Rest_Abstract
     public function login($username, $password)
     {
         $url = "{$this->getBaseUrl()}/api/{$this->_loginBaseUrl}";
-        
+
         $result = $this->_doPostRequest($url, array('username' => $username, 'password' => $password));
-        
+
         if (!$result OR !isset($result['data'])) {
-        	throw new Alfresco_Rest_Exception("Unable to connect to authentication service.");
+            throw new Alfresco_Rest_Exception("Unable to connect to authentication service.");
         }
-        
+
         return $result['data'];
     }
 
     /**
+     * Basic check for authentication service
+     *
+     * POST /alfresco/service/api/ckeck-login
+     *
+     * @param string $username
+     * @param string $password
+     * @return boolean
+     */
+    public function checkLogin($username, $password)
+    {
+        try {
+            $url = "{$this->getBaseUrl()}/api/{$this->_loginBaseUrl}";
+
+            $result = $this->_doPostRequest($url, array('username' => $username, 'password' => $password));
+
+            return true;
+        } catch (Exception $exc) {
+            return false;
+        }
+    }
+
+    /**
      * Logout
-     * 
+     *
      * DELETE /alfresco/service/api/login/ticket/{ticket}
-     * 
+     *
      * @param string $ticket
      * @return array service's response
      */
     public function logout($ticket)
     {
         $url = "{$this->getBaseUrl()}/api/{$this->_loginBaseUrl}/{$this->_loginTicketUrl}/$ticket";
-        
+
         return $this->_doAuthenticatedDeleteRequest($url);
     }
-    
+
     /**
-     * Validates the specified ticket is still valid. 
-     * The ticket may be invalid, or expired, or the user may have been locked out. 
+     * Validates the specified ticket is still valid.
+     * The ticket may be invalid, or expired, or the user may have been locked out.
      * For security reasons this script will not validate the ticket of another user.
-     * 
+     *
      * GET /alfresco/service/api/login/ticket/<ticket>?alf_ticket=<ticket>
-     * 
+     *
      * @return boolean
      */
     public function validate()
     {
         $url = "{$this->getBaseUrl()}/api/{$this->_loginBaseUrl}/{$this->_loginTicketUrl}/{$this->getTicket()}";
         $result = trim($this->_doAuthenticatedGetStringRequest($url));
-        
+
         return (strpos($result, 'TICKET_') > -1);
     }
+
 }
