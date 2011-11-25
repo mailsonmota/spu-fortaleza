81580e40d87956e86cdef4cab65c5cbb004a192e
gilmagno <gilmagno@gmail.com>
2011-11-25 19:36:53 +0000

    Modificação do web script que procura tipo de documento.

--- alfresco/Web Scripts Extensions/org/spu/tipos-documentos/buscar.get.js
+++ alfresco/Web Scripts Extensions/org/spu/tipos-documentos/buscar.get.js
@@ -1,6 +1,21 @@
-model.opcoes = search.luceneSearch(
+resultado_busca_termo = search.luceneSearch(
     'workspace://SpacesStore',
     'PATH:"/cm:generalclassifiable/cm:SPU/cm:Tipo_x0020_de_x0020_Documento//*"'
-    + 'AND ALL:"' + url.templateArgs['termo'] + '*"'
-)
+    + 'AND @cm\\:name:"' + url.templateArgs['termo'] + '*"'
+);
 
+/*nodes_to_exclude = search.luceneSearch('workspace://SpacesStore',
+    'PATH:"/cm:generalclassifiable/cm:SPU/cm:Tipo_x0020_de_x0020_Documento/*"'
+);
+
+nodes_to_exclude_children = nodes_to_exclude.children // como pegar children?
+
+throw nodes_to_exclude_children //
+
+for (var i in nodes_to_exclude_children) {
+    nodes_to_exclude.push(nodes_to_exclude_children[i])
+}*/
+
+//throw nodes_to_exclude[1].name
+
+model.opcoes = resultado_busca_termo
