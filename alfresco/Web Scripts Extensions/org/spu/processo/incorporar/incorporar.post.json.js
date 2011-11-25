973f97407d1174a7aa56ed0a1014a73752ef0bc0
gilmagno <gilmagno@gmail.com>
2011-11-25 14:47:58 +0000

    Implementa incorporação múltipla.

--- alfresco/Web Scripts Extensions/org/spu/processo/incorporar/incorporar.post.json.js
+++ alfresco/Web Scripts Extensions/org/spu/processo/incorporar/incorporar.post.json.js
@@ -1,16 +1,22 @@
 <import resource="/Company Home/Data Dictionary/Scripts/SPU/processo.js">
 
 var principalId = json.get("principal");
-var incorporadoId = json.get("incorporado");
+//var incorporadoId = json.get("incorporado");
+var incorporados = json.get("incorporados");
+incorporados = eval('(' + incorporados + ')');
 
 var principalNode = getNode(principalId);
-var incorporadoNode = getNode(incorporadoId);
+//var incorporadoNode = getNode(incorporadoId);
 
-incorporadoNode.properties['spu:processo.Destino'] = principalId;
-incorporadoNode.save();
+for (var i in incorporados) {
+    var incorporadoNode = getNode(incorporados[i]);
+    incorporadoNode.properties['spu:processo.Destino'] = principalId;
+    incorporadoNode.save();
 
-var workflow = actions.create("start-workflow");
-workflow.parameters.workflowName = "jbpm$spu:incorporarProcesso"; 
-workflow.execute(incorporadoNode);
+    var workflow = actions.create("start-workflow");
+    workflow.parameters.workflowName = "jbpm$spu:incorporarProcesso"; 
+    workflow.execute(incorporadoNode);
+}
 
 model.processo = principalNode;
+
