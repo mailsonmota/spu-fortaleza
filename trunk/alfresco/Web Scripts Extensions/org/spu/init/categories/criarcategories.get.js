2742bacfe3a597c7e9627103a42ffd4d6c5ce2ae
moreira.deivid@gmail.com <moreira.deivid@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2011-11-28 14:08:05 +0000

    add scripts para criar tipos documentos

--- trunk/alfresco/Web Scripts Extensions/org/spu/init/categories/criarcategories.get.js
+++ trunk/alfresco/Web Scripts Extensions/org/spu/init/categories/criarcategories.get.js
@@ -1,3 +1,4 @@
 <import resource="/Company Home/Data Dictionary/Scripts/SPU/criarcategories.js">
-result = criarTodasAsCategories();
+//result = criarTodasAsCategories();
+result = criarCategoryTipoDocumento(); 
 model.mensagem = (result) ? "Categorias criadas com sucesso." : "Ocorreu um erro ao criar as categories.";
