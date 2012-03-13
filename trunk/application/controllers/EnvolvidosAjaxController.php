74dc2a5d75a70def13450bb8b143f90dda7cd316
igor.roben@gmail.com <igor.roben@gmail.com@44544069-6ebe-d841-5405-7e90ffb1003f>
2012-03-13 15:42:42 +0000

    busca envolvido e busca ajax com busca de admin

--- trunk/application/controllers/EnvolvidosAjaxController.php
+++ trunk/application/controllers/EnvolvidosAjaxController.php
@@ -4,17 +4,37 @@ class EnvolvidosAjaxController extends BaseAuthenticatedController
     public function envolvidoAction()
     {
         $this->_helper->layout()->disableLayout();
-        $this->view->manifestante = $this->_getManifestante(str_replace(array(".","-", "/"), "",$this->_getSearchTerm()));
+        $this->view->manifestante = $this->_getManifestante($this->_filterCpfCnpj($this->_getSearchTerm()));
     }
 
     protected function _getManifestante($cpfCnpj)
     {
-        $manifestanteService = new Spu_Service_Manifestante($this->getTicket());
+        $manifestanteService = new Spu_Service_Manifestante($this->_getTicketAdmin());
         return $manifestanteService->getManifestante($cpfCnpj);
     }
 
     protected function _getSearchTerm()
     {
         return $this->getRequest()->getParam('term', null);
+    }
+    
+    private function _filterCpfCnpj($val)
+    {
+        return strlen($val) != 18 ? str_replace(array(".","-"), "",$val) : $val;
+    }
+    
+    private function _getBaseUrl()
+    {
+        $init = new Zend_Config_Ini('../application/configs/application.ini', APPLICATION_ENV);
+        return $init->alfresco->url;
+    }
+
+    private function _getTicketAdmin()
+    {
+        $alfresco = new Alfresco_Rest_Login($this->_getBaseUrl() . "/service");
+        $ticket = $alfresco->login("login", "senha");
+        $alfresco->logout($ticket['ticket']);
+        
+        return $ticket['ticket'];
     }
 }
